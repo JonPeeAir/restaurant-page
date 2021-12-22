0a73cf17d3cf04d23c76fb9b49c0bf7fa947769e
John Pierre Mendoza <johnpierremendoza@gmail.com>
2021-12-22 15:10:48 +0800

    feat: allow users to navigate to the about and contact pages
    note: the html for these pages are dynamically loaded with javascript

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -4,7 +4,28 @@ export default (() => {
 
     function load() {
         clearContent();
-        return;
+        displayAboutPage();
+        window.scrollTo({
+            top: 1,
+            behavior: 'smooth',
+        });
+    }
+
+    function displayAboutPage() {
+        const content = document.getElementById("content");
+
+        const aboutPage = document.createElement("div");
+        aboutPage.id = "about-page";
+        aboutPage.classList.add("page");
+
+        const pageTitle = document.createElement("p");
+        pageTitle.classList.add("page-title");
+        pageTitle.textContent = "About";
+
+        aboutPage.append(pageTitle);
+
+        content.append(aboutPage);
+
     }
 
     return { load };
--- src/ContactPage.js
+++ src/ContactPage.js
@@ -4,7 +4,28 @@ export default (() => {
 
     function load() {
         clearContent();
-        return;
+        displayContactPage();
+        window.scrollTo({
+            top: 1,
+            behavior: 'smooth',
+        });
+    }
+
+    function displayContactPage() {
+        const content = document.getElementById("content");
+
+        const contactPage = document.createElement("div");
+        contactPage.id = "contact-page";
+        contactPage.classList.add("page");
+
+        const pageTitle = document.createElement("p");
+        pageTitle.classList.add("page-title");
+        pageTitle.textContent = "Contact";
+
+        contactPage.append(pageTitle);
+
+        content.append(contactPage);
+
     }
 
     return { load };
