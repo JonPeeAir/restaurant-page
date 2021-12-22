e83b5a20641d3f41514c405a6ae5f876d1233d27
John Pierre Mendoza <johnpierremendoza@gmail.com>
2021-12-22 12:40:19 +0800

    feat: add the logic that loads the menupage html with javascript

--- src/MenuPage.js
+++ src/MenuPage.js
@@ -4,6 +4,24 @@ export default (() => {
 
     function load() {
         clearContent();
+        displayMenuPage();
+    }
+
+    function displayMenuPage() {
+        const content = document.getElementById("content");
+
+        const menuPage = document.createElement("div");
+        menuPage.id = "menu-page";
+        menuPage.classList.add("page");
+
+        const pageTitle = document.createElement("p");
+        pageTitle.classList.add("page-title");
+        pageTitle.textContent = "Menu";
+
+        menuPage.append(pageTitle);
+
+        content.append(menuPage);
+
     }
 
     return { load };
